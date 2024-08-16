316bcc35cc8a2af83ed3cfc583509f6fa9c34051
vitgavrilchenko <vitgavrilchenko@gmail.com>
2024-08-16 13:43:06 +0300

    validation+new annotation

---
+++ src/main/java/ru/vitprojects/springdemo/mvc/Customer.java
@@ -0,0 +1,63 @@
+package ru.vitprojects.springdemo.mvc;
+
+import jakarta.validation.constraints.*;
+import ru.vitprojects.springdemo.mvc.validation.CourseCode;
+
+public class Customer {
+
+    private String firstName;
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "minimum 0")
+    @Max(value = 10, message = "maximum 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+}
---
+++ src/main/java/ru/vitprojects/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,21 @@
+package ru.vitprojects.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+
+
+        return theCode == null || theCode.startsWith(coursePrefix);
+    }
+}
